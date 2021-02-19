53801bdf9209f9ab5ba8e33ce4df58741820f49f
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-19 05:47:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -192,17 +192,3 @@ def player_stats(player_name)
   end
 end
             
-def big_shoe_rebounds
-  shoe = 0 
-  rebounds = 0 
-  game_hash.each do |location, team_data|
-    team_data[:players].each do |player|
-      if player[:shoe] > shoe
-        shoe = player[:shoe]
-        rebounds = player[:rebounds]
-      end
-      end
-    end
-  end
-  return rebounds
-end
