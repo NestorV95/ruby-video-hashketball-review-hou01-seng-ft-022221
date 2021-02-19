9d658aa4279f42e4957967b94aea0757a0347c86
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-19 05:21:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,7 +130,7 @@ end
  
 
 def num_points_scored(player_name)
- players = game_hash[:home][:players].merge(game_hash[:away][:players])
+ players= game_hash[:home][:players].merge(game_hash[:away][:players])
   return players[player_name][:points]
 end
 
